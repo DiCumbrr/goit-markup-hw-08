792aa92d863c75909c513fed9035cc63bfba3e1b
DiCumbrr <104522913+DiCumbrr@users.noreply.github.com>
2022-07-04 01:08:52 +0300

    hw8

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,19 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    toggleMenu: document.querySelector('[toggle-btn]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
+    button: document.querySelector('.toggle-btn'),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  // refs.openMenuBtn.addEventListener('click', toggleMenu);
+  // refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.toggleMenu.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.menu.classList.toggle('is-open');
+    refs.button.classList.toggle('is-open');
     refs.body.classList.toggle('no-scroll');
   }
 })();
